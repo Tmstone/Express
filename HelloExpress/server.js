c6fd2b5bfd852810cef8b6e674d3cc38ff91de97
Terence Stone <terence.stone25@gmail.com>
2020-03-04 08:42:17 -0500

    updated server.js

--- HelloExpress/server.js
+++ HelloExpress/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views');
 app.get('/', (request, response) => {
     response.send("Hello Express");
+    app.set('view engine', 'ejs');
+    app.set('views', __dirname + '/views');
+    app.use(express.static(__dirname + '/static'));
 });
-app.use(express.static(__dirname + '/static'));
+
 app.listen(8000, () => console.log("Listening on port 8000"));
