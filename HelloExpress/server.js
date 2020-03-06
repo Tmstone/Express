6cda8db871ca28f362c03063a1ae2e5ca605b983
Terence Stone <terence.stone25@gmail.com>
2020-03-05 23:42:49 -0500

    adding new views to server.js

--- HelloExpress/server.js
+++ HelloExpress/server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', (request, response) => {
     response.send("Hello Express");
-    app.set('view engine', 'ejs');
-    app.set('views', __dirname + '/views');
-    app.use(express.static(__dirname + '/static'));
 });
+app.get("/users", (req, res) => {
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"}, 
+        {name: "Jay", email: "jay@codingdojo.com"}, 
+        {name: "Brendan", email: "brendan@codingdojo.com"}, 
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    res.render('users', {users: users_array});
+})
 
 app.listen(8000, () => console.log("Listening on port 8000"));
