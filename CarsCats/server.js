c033440339b31085a543544283a82dbffe364155
Terence Stone <terence.stone25@gmail.com>
2020-03-13 14:20:44 -0400

    updated views and server.js

--- CarsCats/server.js
+++ CarsCats/server.js
@@ -3,13 +3,20 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+//app.set('views', ___dirname + '/views/images');
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (request, response) => {
     response.send('Cars and Cats');
 });
 app.get('/cars', (request, response) => {
-    //response.send(cars.html);
+    response.render('cars');
+})
+app.get('/cats', (request, response) => {
+    response.render('cats');
+})
+app.get('/form', (request, response) => {
+    response.render('form');
 })
 
 app.listen(8000, () => console.log('listening on port 8000'));
