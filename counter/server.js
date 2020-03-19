1451d29f73897772303499c04d9d3aff5b20ced1
Terence Stone <terence.stone25@gmail.com>
2020-03-18 23:59:37 -0400

    added session

--- counter/server.js
+++ counter/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+const session = require('express-session');
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (request, response) => {
