c230cfb391596b03619be3b3a600e0a95888bf78
Terence Stone <terence.stone25@gmail.com>
2020-03-19 00:04:19 -0400

    updated root root

--- counter/server.js
+++ counter/server.js
@@ -12,6 +12,7 @@ app.use(session({
 app.use(express.static(__dirname + '/static'));
 
 app.get('/', (request, response) => {
-    response.send('Counter');
+    console.log("Value of counter in session ", req.session.counter)
+    response.render('index', {title: "Counter"});
 });
 app.listen(8000, () => console.log('listening on port 8000'));
