d9eb8f271cdf563cf6ca7f07866e9250c392ea25
Terence Stone <terence.stone25@gmail.com>
2020-03-23 22:41:42 -0400

    cleaning up server

--- counter/server.js
+++ counter/server.js
@@ -2,25 +2,21 @@ const session = require('express-session');
 const express = require('express');
 const app = express();
 
-app.use(session({
+app.use (session({
   secret: 'keyboardkitteh',
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 60000 }
-}))
+}));
 
 const counter = 0;
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
-
-
 //app.use(express.static(__dirname + '/static'));
 
 app.get('/', (request, response) => {
-    console.log("Value of counter in session ", request.session.count);
-    //console.log('Counter value:' , counter);
+    console.log("Value of counter in session ", request.session.counter);
     response.render('index', {counter: count(request) });
-
 });
 
 /*
@@ -31,7 +27,6 @@ app.post('/', (request, response) => {
 */
 function count (request) {
   return request.session.counter = request.session.counter ? request.session.counter + 1: 1;
-  
 }
 
 app.listen(8000, () => console.log('listening on port 8000'));
