7f06695402f9dfb444eb7c26b8f0e2452b248870
Terence Stone <terence.stone25@gmail.com>
2020-03-31 23:27:43 -0400

    added socket functions

--- sfr/server.js
+++ sfr/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const port = process.env.port || 8000;
-//const io = require(socket.io)(server);
+const io = require('socket.io')();
 const app = express();
-var num = Math.floor(Math.random() * 1000) + 1 ;
+//var num = Math.floor(Math.random() * 1000) + 1 ;
+var counter = 0;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get('/', (req, res) => {
+    var num = Math.floor(Math.random() * 1000) + 1 ;
     console.log(num);
     res.render('index');
 });
 
 //socket function here 
+io.on('connection', function(socket) {
+    socket.emit('greeting', {msg: 'Greetings, from server Node, brought to you by Sockets! - Server'});
+    socket.on('thankyou', function (data) {
+        console.log(data.msg);
+    })
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
