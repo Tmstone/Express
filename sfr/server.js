013be3993ee7c97b28d417976730d2994bab5c2b
Terence Stone <terence.stone25@gmail.com>
2020-04-02 23:10:15 -0400

    updated server and index.ejs, socket works

--- sfr/server.js
+++ sfr/server.js
@@ -1,25 +1,21 @@
 const express = require('express');
-const port = process.env.port || 3000;
-const io = require('socket.io')();
+const port = process.env.port || 8000;
 const app = express();
 //var num = Math.floor(Math.random() * 1000) + 1 ;
 var counter = 0;
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+//app.use(express.static(__dirname + '/static'));
+const server = app.listen(port, () => console.log(`Listening on port ${port}`));
+const io = require('socket.io')(server);
 
+//socket function here 
+io.on('connection', socket => {
+    console.log('incoming socket connection');
+})
 app.get('/', (req, res) => {
     var num = Math.floor(Math.random() * 1000) + 1 ;
     console.log(num);
     res.render('index');
 });
-
-//socket function here 
-io.on('connection', function(socket) {
-    socket.emit('greeting', {msg: 'Greetings, from server Node, brought to you by Sockets! - Server'});
-    socket.on('thankyou', function (data) {
-        console.log(data.msg);
-    })
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
