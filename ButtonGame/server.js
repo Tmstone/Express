f25cf124b5f171dee7a9db9224714dc69cbab479
Terence Stone <terence.stone25@gmail.com>
2020-04-05 21:22:31 -0700

    modified template

--- ButtonGame/server.js
+++ ButtonGame/server.js
@@ -10,7 +10,7 @@ const io = require('socket.io')(server);
 
 io.on('connection', socket => {
     console.log('Incomming socket connection');
-})
+});
 
 app.get('/', (req, res) => {
     //console.log(num);
