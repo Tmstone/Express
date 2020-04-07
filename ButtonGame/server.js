4ce7962abc71da8e21e8ce87f9450218d75000d9
Terence Stone <terence.stone25@gmail.com>
2020-04-06 23:15:49 -0400

    added global update function

--- ButtonGame/server.js
+++ ButtonGame/server.js
@@ -8,18 +8,22 @@ app.set('views', __dirname + '/views');
 const server = app.listen(port, ()=> console.log(`Listening on port ${port}`));
 const io = require('socket.io')(server);
 
+let count = 0;
 io.on('connection', socket => {
-    let count = 0;
     console.log('Incomming socket connection');
 
     socket.on('button_clicked', function() {
-        io.emit('numberUpdate', ++count)
+        //io.emit('numberUpdate', ++count)
+        updateNumber(++count);
     });
     socket.on('reset', function () {
-        count = 0;
-        io.emit('numberUpdate', count);
+        updateNumber(count = 0);
+        //io.emit('numberUpdate', count);
     })
 });
+function updateNumber(num) {
+    io.emit('numberUpdate', num);
+}
 
 app.get('/', (req, res) => {
     //console.log(num);
