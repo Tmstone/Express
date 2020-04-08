31849d8f4880db0b157bac2366816236d5b2d92f
Terence Stone <terence.stone25@gmail.com>
2020-04-08 00:57:53 -0400

    changed server needs

--- groupChat/server.js
+++ groupChat/server.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const port = process.env.port || 5000;
-const session = require('express-session');
+//const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
-
+/*
 app.use(session({
     secret: 'berlinstation',
     resave: false,
     saveUninitialized: true,
     cookie: {maxAge: 6000}
 }));
-
+*/
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 const io = require('socket.io')(server);
 
 
 app.get('/', (req, res) => {
-    console.log('Value of name in session: ', req.session);
+    //console.log('Value of name in session: ', req.session);
     res.render('index');
 });
+
 
 //socket function here
 io.on('connection', socket => {
@@ -31,10 +32,4 @@ io.on('connection', socket => {
          
     })
 
-})
-/*
-app.post('/chat', (req, res) => {
- req.session.name = req.body.name;
- res.render('chat');
 });
-*/
