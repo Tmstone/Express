4f4917f713521f44d0a3181076d336924d48a44d
Terence Stone <terence.stone25@gmail.com>
2020-04-08 00:00:36 -0400

    added sesssion to server

--- groupChat/server.js
+++ groupChat/server.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const port = process.env.port || 5000;
-
+const session = require('express-session');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
 
+app.use(session({
+    secret: 'berlinstation',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {maxAge: 6000}
+}));
+
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 const io = require('socket.io')(server);
+
 
 app.get('/', (req, res) => {
     res.render('index');
