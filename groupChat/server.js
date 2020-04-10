f074c10a006b6f7c4ba4aa720f806753a69d9813
Terence Stone <terence.stone25@gmail.com>
2020-04-09 23:29:29 -0400

    updated server

--- groupChat/server.js
+++ groupChat/server.js
@@ -1,25 +1,23 @@
 const express = require('express');
 const port = process.env.port || 5000;
-//const session = require('express-session');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use(express.urlencoded({extended: true}));
-/*
-app.use(session({
-    secret: 'berlinstation',
-    resave: false,
-    saveUninitialized: true,
-    cookie: {maxAge: 6000}
-}));
-*/
+
+var users = {};
+var message = {};
+
+
+
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 const io = require('socket.io')(server);
 
 
 app.get('/', (req, res) => {
-    //console.log('Value of name in session: ', req.session);
+    console.log('page rendered');
     res.render('index');
 });
 
