89144940429ce0ce834155a3d7d04f9bc2e04a6d
Terence Stone <terence.stone25@gmail.com>
2020-05-26 22:50:36 -0400

    updating server

--- firstMongo/server.js
+++ firstMongo/server.js
@@ -4,7 +4,6 @@ const { Schema } = mongoose;
 
 mongoose.connect('mongodb://localhost/animals', {
     useNewUrlParser: true
-
 });
 
 mongoose.connection.on('connected', () => console.log('Mongodb connected'));
