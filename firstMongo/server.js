5a3b42dd8af284a3d4a886841e6b88e761c9687b
Terence Stone <terence.stone25@gmail.com>
2020-06-01 00:05:23 -0400

    Adding animal db with data

--- firstMongo/server.js
+++ firstMongo/server.js
@@ -3,12 +3,32 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 
-mongoose.connect('mongodb://localhost/animals', {
-    useNewUrlParser: true
-
-});
+mongoose.connect('mongodb://localhost/animals', { useUnifiedTopology : true , useNewUrlParser: true });
 
 mongoose.connection.on('connected', () => console.log('Mongodb connected'));
+
+
+const AnimalSchema = new Schema ({
+    name: String,
+    age: Number,
+    legs: Number,
+    isPet: Boolean,
+});
+
+const Animal = mongoose.model('Animal', AnimalSchema );
+
+const animal = new Animal ({
+    name: 'Spot',
+    age: 3,
+    legs: 4,
+    isPet: true
+})
+
+//console.log(animal);
+
+animal.save(function (...content) {
+console.log(content)
+});
 
 // destructuring mongoose
 /*
