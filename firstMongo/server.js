1fead126dfdefa8c5bac3cd4e9930471cccb834e
Terence Stone <terence.stone25@gmail.com>
2020-05-31 23:31:51 -0400

    Destructuring the Mongoose object

--- firstMongo/server.js
+++ firstMongo/server.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 
-const { Schema } = mongoose;
+//const { Schema } = mongoose;
 
+/*
 mongoose.connect('mongodb://localhost/animals', {
     useNewUrlParser: true
+
 });
 
 mongoose.connection.on('connected', () => console.log('Mongodb connected'));
+*/
+// destructuring mongoose
+const m = {
+    Schema: function () {},
+    models: {}
+};
+
+const { Schema, models } = m;
+
+console.log('Schema', Schema, models);
