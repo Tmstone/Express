c5ccef03141097577c7241d93294e7005c1e50fe
Terence Stone <terence.stone25@gmail.com>
2020-05-31 23:36:30 -0400

    Destructuring the Mongoose object with altSchema

--- firstMongo/server.js
+++ firstMongo/server.js
@@ -1,21 +1,23 @@
 const mongoose = require('mongoose');
 
-//const { Schema } = mongoose;
+const { Schema } = mongoose;
 
-/*
+
 mongoose.connect('mongodb://localhost/animals', {
     useNewUrlParser: true
 
 });
 
 mongoose.connection.on('connected', () => console.log('Mongodb connected'));
-*/
+
 // destructuring mongoose
+/*
 const m = {
     Schema: function () {},
     models: {}
 };
 
-const { Schema, models } = m;
+const { models, Schema: AltSchema } = m;
 
-console.log('Schema', Schema, models);
+console.log('Schema', Schema, models, AltSchema);
+*/
